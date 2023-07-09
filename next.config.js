ec7f96b2757894c515b155ceceb25841dff748b0
Chitvan14 <chitvangarg14@gmail.com>
2023-07-09 12:05:02 +0530

    Updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
