cf2c5fbda4af9d15eead1b1909287aa397cfa673
Chitvan14 <chitvangarg14@gmail.com>
2023-07-09 11:43:48 +0530

    Bug fix

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -124,7 +124,7 @@ export default function ProfilePage() {
                         className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         href={`/profile/${data._id}`}
                       >
-                        Let's Check
+                        Lets Check
                       </Link>
                     )}
                   </div>
